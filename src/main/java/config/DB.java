a7de70ed76b412d9f365cfbd88434a40d431cb6d
Your Name <kinaiatovnakoo@gmail.com>
2026-01-19 16:08:57 +0500

    DB

--- src/main/java/config/DB.java
+++ src/main/java/config/DB.java
@@ -4,9 +4,9 @@ import java.sql.*;
 
 public class DB {
     public static Connection getConnection() throws SQLException {
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        String user = "postgres";
-        String pass = "nako9986"; //
+        String url = "jdbc:postgresql://localhost:5432/postgres ";
+        String user = "user_name";
+        String pass = "password"; //
         return DriverManager.getConnection(url, user, pass);
     }
 }
