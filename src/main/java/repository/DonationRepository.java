bcfbcedd6acb1fe2388ed0459e6ee69f7d4c3f77
Your Name <kinaiatovnakoo@gmail.com>
2026-01-19 16:33:33 +0500

    donation repository

---
+++ src/main/java/repository/DonationRepository.java
@@ -0,0 +1,19 @@
+package repository;
+
+import config.DB;
+import java.sql.*;
+
+public class DonationRepository {
+
+    public void createDonation(int donorId, int campaignId, double amount) throws SQLException {
+        String sql = "INSERT INTO donation(donor_id, campaign_id, amount) VALUES (?, ?, ?)";
+
+        try (Connection con = DB.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setInt(1, donorId);
+            ps.setInt(2, campaignId);
+            ps.setDouble(3, amount);
+            ps.executeUpdate();
+        }
+    }
+}
--- src/main/java/repository/DonorRepository.java
+++ src/main/java/repository/DonorRepository.java
@@ -1,4 +1,4 @@
-package controller;
+package repository;
 
 public class DonorRepository {
 }
